a393b122b3032d5ab4d91ae2c17f823ecdcfc99d
nanbanban <ryota05basuke@icloud.com>
2023-10-09 16:53:34 +0900

    データ2

--- database/migrations/2023_09_27_103021_create_comments_table.php
+++ database/migrations/2023_09_27_103021_create_comments_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); // 外部キー制約
             $table->text('content');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            // $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
