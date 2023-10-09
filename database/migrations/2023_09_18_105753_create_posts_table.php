c14f26c8f0e3b9b549b43b1f02e36fa7286c1943
nanbanban <ryota05basuke@icloud.com>
2023-10-09 16:59:16 +0900

    データ3

--- database/migrations/2023_09_18_105753_create_posts_table.php
+++ database/migrations/2023_09_18_105753_create_posts_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            // $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
